24504cda91cfa543b10111704758303e4924cf5e
Administrator <Administrator@zhangtianxi>
2015-10-19 17:12:07 +0800

    上传会员的实体类

---
+++ pot/src/com/pro/domain/Member.java
@@ -0,0 +1,23 @@
+package com.pro.domain;
+
+public class Member {
+    private int member_id;
+    
+    private String member_name;
+    
+    private String member_type;
+    
+    private Member_Set member_set;
+    
+    private int member_balance;
+    
+    private int member_integral;
+    
+    private int member_expense;
+    
+    private String member_birthday;
+    
+    private int member_tel;
+    
+    private String member_datetime;
+}
---
+++ pot/src/com/pro/domain/Member_Set.java
@@ -0,0 +1,9 @@
+package com.pro.domain;
+
+public class Member_Set {
+    private int grade_id;
+    
+    private String member_grade;
+    
+    private int member_rate;
+}
