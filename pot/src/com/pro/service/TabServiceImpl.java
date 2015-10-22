ea91874ff79360effb329edebea52ecd61cc09f1
Administrator <Administrator@192.168.1.155>
2015-10-22 14:49:07 +0800

    xml

--- pot/src/com/pro/service/TabServiceImpl.java
+++ pot/src/com/pro/service/TabServiceImpl.java
@@ -9,8 +9,8 @@ import com.pro.dao.TabDaoImpl;
 public class TabServiceImpl extends BaseServiceImpl implements TabService {
 	TabDao tabDao = new TabDaoImpl();
 	
-	public Map<String,Object> pageTab(int currentPage,int pageSize){
-		return tabDao.pageTab(currentPage, pageSize);
+	public Map<String,Object> pageTab(int rows,int page){
+		return tabDao.pageTab(page, rows);
 	}
 	
 }
